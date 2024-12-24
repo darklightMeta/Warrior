0bb9b33b57dbc71c1477b928c01a2737de32a99c
darklightMeta <5spa.dispatch@gmail.com>
2024-12-25 01:22:51 +1030

    Enhanced Input Control added to Bird Class

--- Source/Warrior/Private/Pawns/Bird.cpp
+++ Source/Warrior/Private/Pawns/Bird.cpp
@@ -2,23 +2,78 @@
 
 
 #include "Pawns/Bird.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h" 
+#include "EnhancedInputComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/PlayerController.h"
 
-// Sets default values
+
 ABird::ABird()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	
 	PrimaryActorTick.bCanEverTick = true;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	RootComponent = Capsule;
+	Capsule->SetCapsuleHalfHeight(20.f);
+	Capsule->SetCapsuleRadius(15.f);
+	//Capsule->SetCollisionProfileName("Pawn");
+	//Capsule->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	//Capsule->SetCollisionResponseToAllChannels(ECR_Ignore);
+	//Capsule->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	//Capsule->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
+	BirdMesh->SetupAttachment(GetRootComponent());
+	//BirdMesh->SetRelativeLocation(FVector(0.f, 0.f, 10.f));
+	//BirdMesh->SetRelativeRotation(FRotator(0.f, 0.f, 0.f));
+	//BirdMesh->SetRelativeScale3D(FVector(1.f, 1.f, 1.f));
+	//BirdMesh->SetCollisionProfileName("Pawn");
+	//BirdMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	//BirdMesh->SetCollisionResponseToAllChannels(ECR_Ignore);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 
 }
 
-// Called when the game starts or when spawned
+
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
+
+	
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(BirdMappingContext, 0);
+		}
+	}
 	
 }
 
-// Called every frame
+void ABird::MoveForward(float Value)
+{
+	//AddActorWorldOffset(FVector(0.f, 0.f, Value * 100.f));
+	UE_LOG(LogTemp, Warning, TEXT("MoveForward: %f"), Value);
+}
+
+void ABird::Move(const FInputActionValue& Value)
+{
+	const float DirectionValue = Value.Get<float>();
+
+	if  (Controller && (DirectionValue != 0.f))
+	{
+		FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, DirectionValue);
+	}
+}
+
+
 void ABird::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -29,6 +84,14 @@ void ABird::Tick(float DeltaTime)
 void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+	}
+
+	
+
 
 }
 
--- Source/Warrior/Public/Pawns/Bird.h
+++ Source/Warrior/Public/Pawns/Bird.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "Bird.generated.h"
+
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class WARRIOR_API ABird : public APawn
@@ -12,18 +18,36 @@ class WARRIOR_API ABird : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
+
 	ABird();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	
+	virtual void BeginPlay() override;
+	void MoveForward(float Value);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* BirdMappingContext;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveAction;
+
+	void Move(const FInputActionValue& Value);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UCapsuleComponent* Capsule;
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* BirdMesh;
+	
+
+	
 
 };
+
+
